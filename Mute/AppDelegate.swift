ab02c32ea97560608c9ac9208f38d1b43233b2ac
w1874 <wangyifan@live.hk>
2017-07-10 20:37:59 +0800

    Initial Commit

---
+++ Mute/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mute
+//
+//  Created by 王一凡 on 2017/7/10.
+//  Copyright © 2017年 王一凡. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mute/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mute
+//
+//  Created by 王一凡 on 2017/7/10.
+//  Copyright © 2017年 王一凡. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
